2f94779e7f523d8f36865cdd47350441defd58c2
Suman Kumar ~BHUTUU <raj259942@gmail.com>
2021-12-23 09:46:00 +0530

    removed some unusuals things

--- programs/hashencode/hash.sh
+++ programs/hashencode/hash.sh
@@ -14,17 +14,17 @@ R0="\033[00m"   R1="\033[1;00m"
 printf "\033[3;4;37mTSc\033[0;0;00m ${S7}Enter(${S1}text/to/${args}/hash${S7})> ${R0}"
 read text
 if [[ $args == *'md5'* ]]; then
-hash=$(echo "${text}" | md5sum | awk '{print $1}')
+hash=$(printf "${text}" | md5sum | awk '{print $1}')
 elif [[ $args == *'sha1'* ]]; then
-hash=$(echo "${text}" | sha1sum | awk '{print $1}')
+hash=$(printf "${text}" | sha1sum | awk '{print $1}')
 elif [[ $args == *'sha224'* ]]; then
-hash=$(echo "${text}" | sha224sum | awk '{print $1}')
+hash=$(printf "${text}" | sha224sum | awk '{print $1}')
 elif [[ $args == *'sha256'* ]]; then
-hash=$(echo "${text}" | sha256sum | awk '{print $1}')
+hash=$(printf "${text}" | sha256sum | awk '{print $1}')
 elif [[ $args == *'sha384'* ]]; then
-hash=$(echo "${text}" | sha384sum | awk '{print $1}')
+hash=$(printf "${text}" | sha384sum | awk '{print $1}')
 elif [[ $args == *'sha512'* ]]; then
-hash=$(echo "${text}" | sha512sum | awk '{print $1}')
+hash=$(printf "${text}" | sha512sum | awk '{print $1}')
 else
 hash=$(printf "${S2}[${S1}!${S2}]${S4}Lol! you are trying to copy my program${R0} 😂😂\n")
 fi
